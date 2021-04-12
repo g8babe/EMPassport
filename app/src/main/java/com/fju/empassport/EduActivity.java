f923d5363bc47e23802dfc5e49c11e26b5d4c7b5
Jasmine <jasmine231031@gmail.com>
2021-04-12 20:00:16 +0800

    3rd

---
+++ app/src/main/java/com/fju/empassport/EduActivity.java
@@ -0,0 +1,14 @@
+package com.fju.empassport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EduActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edu);
+    }
+}
